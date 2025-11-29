02fdaecbdbbbaff13f7e3574fb942efe94f8cffe
ericksjp <ericksjp703@gmail.com>
2025-11-28 21:11:28 -0300

    feat: configuração inicial do projeto charger-manager
    - cria aplicação Spring Boot com HealthController
    - adiciona configuração básica no application.yaml

---
+++ charger-manager/src/main/java/com/ifpb/charger_manager/ChargerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.charger_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargerManagerApplication.class, args);
+	}
+
+}
---
+++ charger-manager/src/test/java/com/ifpb/charger_manager/ChargerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.charger_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
