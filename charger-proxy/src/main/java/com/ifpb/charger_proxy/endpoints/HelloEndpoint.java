d500433f353f5cad5f10dda4396701b96f743825
ericksjp <ericksjp703@gmail.com>
2025-11-30 22:36:54 -0300

    refactor(proxy): renomeia e atualiza schemas SOAP e endpoint

--- charger-proxy/src/main/java/com/ifpb/charger_proxy/endpoints/HelloEndpoint.java
+++ charger-proxy/src/main/java/com/ifpb/charger_proxy/endpoints/HelloEndpoint.java
@@ -1,9 +1,11 @@
 package com.ifpb.charger_proxy.endpoints;
 
-import com.ifpb.charger_proxy.schemas.GetMessageResponse;
+import com.ifpb.charger_proxy.schemas.MessageRequest;
+import com.ifpb.charger_proxy.schemas.MessageResponse;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import lombok.RequiredArgsConstructor;
@@ -13,12 +15,22 @@ import lombok.RequiredArgsConstructor;
 public class HelloEndpoint {
     private static final String NAMESPACE_URI = "http://ifpb.com/charger-proxy";
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getMessageResponse")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "MessageRequest")
     @ResponsePayload
-    public GetMessageResponse handleGetMessage() {
-        GetMessageResponse response = new GetMessageResponse();
-        response.setMessage("hello!");
+    public MessageResponse handleGetMessage(@RequestPayload MessageRequest request) {
+        String message = request.getMessage();
+
+        MessageResponse response = new MessageResponse();
+        response.setMessage(String.format("Hello %s! I am %s.", message, getHostname()));
         return response;
+    }
+
+    private String getHostname() {
+        try {
+            return System.getenv("HOSTNAME");
+        } catch (Exception e) {
+            return "space";
+        }
     }
 }
 
