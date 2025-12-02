89a7232de2a4ee6d1da7a92603a3828b9837d9a7
ericksjp <ericksjp703@gmail.com>
2025-12-02 06:23:52 -0300

    refactor(setup): move etapa de migração do banco para apos o provisionamento da stack no start.sh

--- start.sh
+++ start.sh
@@ -17,9 +17,6 @@ cd ../charger-manager
 cd ../charger-proxy
 ./mvnw jib:build -Djib.to.image=192.168.56.32:5000/charger-proxy:latest
 
-# aplica migrations ao bd que esta rodando no manager
-PORT=8080 DB_URL=jdbc:postgresql://192.168.56.32:5432/chargerdb DB_USER=postgrau DB_PASSWORD=postgrau ./mvnw flyway:migrate
-
 # copiar o arquivo de stack para o diretório compartilhado
 cd ..
 cp stack.ex.yml vagrant-env/shared/stacks/stack.yml
@@ -27,3 +24,9 @@ cp stack.ex.yml vagrant-env/shared/stacks/stack.yml
 # provisionar o manager1 com a stack que vai inicializar os serviços
 cd vagrant-env
 vagrant provision manager1 --provision-with start-stack
+
+sleep 10
+
+# aplica migrations ao bd que esta rodando no manager
+cd ../charger-proxy
+PORT=8080 DB_URL=jdbc:postgresql://192.168.56.32:5432/chargerdb DB_USER=postgrau DB_PASSWORD=postgrau ./mvnw flyway:migrate
