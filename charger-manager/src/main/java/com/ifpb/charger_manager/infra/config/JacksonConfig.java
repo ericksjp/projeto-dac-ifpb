92effe321513ccc7a42fa7bbf95b87d23847391f
ericksjp <ericksjp703.gmail.com>
2026-01-31 21:27:08 -0300

    feat(manager): adiciona bean do michael jackson

---
+++ charger-manager/src/main/java/com/ifpb/charger_manager/infra/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.ifpb.charger_manager.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper;
+    }
+}
+
